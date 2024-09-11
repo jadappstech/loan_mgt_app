080d781201eaee5999a1fde1a087a2cd5200bc60
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2024-09-11 13:29:48 +0100

    edited based on review from @unclegoki

---
+++ user/payment-history.php
@@ -0,0 +1,278 @@
+<?php include_once "./includes/header.php"?>
+
+<body>
+  <!-- header start -->
+  <header class="section-t-space">
+    <div class="custom-container">
+      <div class="header-panel">
+        <a href="landing.php" class="back-btn">
+          <i class="icon" data-feather="arrow-left"></i>
+        </a>
+        <h2>Payment History</h2>
+      </div>
+    </div>
+  </header>
+  <!-- header end -->
+
+  <!-- person transaction list section starts -->
+  <section class="section-b-space">
+    <div class="custom-container">
+      <div class="title">
+        <h2 class="light-text fw-normal">1st January - 30nd June</h2>
+      </div>
+
+      <ul class="transaction-list">
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img" style="background-color: #f3000099">
+              <i class="icon" data-feather="arrow-up-right"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Project Tip</h5>
+                </a>
+                <h3 class="success-color">$30.<span>99</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">20 January, 2023</h6>
+                <h5 class="fw-normal light-text">8:45 am</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="gift"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Wedding Gift</h5>
+                </a>
+                <h3 class="error-color">$12.<span>10</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">13 February, 2023</h6>
+                <h5 class="fw-normal light-text">5:45 pm</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="repeat"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Transfer</h5>
+                </a>
+                <h3 class="success-color">$100.<span>99</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">10 March, 2023</h6>
+                <h5 class="fw-normal light-text">3:00 pm</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="layers"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Commission</h5>
+                </a>
+                <h3 class="error-color">$6.<span>20</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">3 April, 2023</h6>
+                <h5 class="fw-normal light-text">2:30 am</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="shopping-bag"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Buy Fruit</h5>
+                </a>
+                <h3 class="success-color">$5.<span>40</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">16 May, 2023</h6>
+                <h5 class="fw-normal light-text">10:35 am</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="coffee"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Coffee Treat</h5>
+                </a>
+                <h3 class="error-color">$10.<span>49</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">28 May, 2023</h6>
+                <h5 class="fw-normal light-text">5:15 pm</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+      <div class="title">
+        <h2 class="light-text fw-normal">1st January - 30nd June</h2>
+      </div>
+      <ul class="transaction-list">
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="command"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Project Tip</h5>
+                </a>
+                <h3 class="success-color">$30.<span>99</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">20 January, 2023</h6>
+                <h5 class="fw-normal light-text">8:45 am</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="gift"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Wedding Gift</h5>
+                </a>
+                <h3 class="error-color">$12.<span>10</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">13 February, 2023</h6>
+                <h5 class="fw-normal light-text">5:45 pm</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="repeat"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Transfer</h5>
+                </a>
+                <h3 class="success-color">$100.<span>99</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">10 March, 2023</h6>
+                <h5 class="fw-normal light-text">3:00 pm</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="layers"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Commission</h5>
+                </a>
+                <h3 class="error-color">$6.<span>20</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">3 April, 2023</h6>
+                <h5 class="fw-normal light-text">2:30 am</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="shopping-bag"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Buy Fruit</h5>
+                </a>
+                <h3 class="success-color">$5.<span>40</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">16 May, 2023</h6>
+                <h5 class="fw-normal light-text">10:35 am</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li class="w-100">
+          <div class="transaction-box">
+            <div class="transaction-img">
+              <i class="icon" data-feather="coffee"></i>
+            </div>
+            <div class="transaction-details">
+              <div class="d-flex justify-content-between align-items-center w-100">
+                <a href="transfer-person.php">
+                  <h5 class="transaction-item fw-normal dark-text">Coffee Treat</h5>
+                </a>
+                <h3 class="error-color">$10.<span>49</span></h3>
+              </div>
+              <div class="d-flex justify-content-between align-items-center w-100 mt-1">
+                <h6 class="fw-normal light-text">28 May, 2023</h6>
+                <h5 class="fw-normal light-text">5:15 pm</h5>
+              </div>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </section>
+  <!-- person transaction list section end -->
+
+  <!-- feather js -->
+  <script src="assets/js/feather.min.js"></script>
+  <script src="assets/js/custom-feather.js"></script>
+
+  <!-- bootstrap js -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+  <!-- script js -->
+  <script src="assets/js/script.js"></script>
+</body>
+
+
+<!-- Mirrored from themes.pixelstrap.net/pwa/mpay/view-transaction.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Sep 2024 05:08:44 GMT -->
+</html>
