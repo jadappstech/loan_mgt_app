239feb7f9312e94e3a6f47482392ef72e548d586
JideonwoFelix <99179966+JideonwoFelix@users.noreply.github.com>
2024-09-19 12:00:59 +0100

    process for business loan

---
+++ user/loan-application-business-loan.php
@@ -0,0 +1,75 @@
+<?php include_once "./includes/header.php"?>
+<body>
+  <!-- side bar start -->
+  <?php include_once "./includes/sidebar.php"?>
+  <!-- side bar end -->
+
+
+  <!-- header start -->
+  <header class="section-t-space">
+    <div class="custom-container">
+      <div class="header-panel">
+        <a href="all-applications.php" class="back-btn">
+          <i class="icon" data-feather="arrow-left"></i>
+        </a>
+        <h2></h2>
+
+        <!-- <div class="dropdown">
+          <a href="#" class="back-btn" role="button" data-bs-toggle="dropdown">
+            <i class="icon" data-feather="settings"></i>
+          </a>
+
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Mute notification</a></li>
+            <li><a class="dropdown-item" href="#">Mark as all read</a></li>
+            <li><a class="dropdown-item" href="#">Remove all</a></li>
+          </ul>
+        </div> -->
+      </div>
+    </div>
+  </header>
+  <!-- header end -->
+  <!-- bottom navbar start -->
+  <!-- <?php include_once "./includes/navbar.php"; ?> -->
+  <!-- bottom navbar end -->
+
+  <section class="section-b-space" style="padding-bottom: 90px; margin: 20px">
+    <div class="card text-center" style="width: 18rem; margin: auto">
+      <div class="card-body">
+        <img src="assets/images/customer-care.png" alt="" style="width: 100%">
+        <h5 class="card-title" style="font-weight: 800; font-size: 30px">You Have Completed The First Step</h5>
+        <!-- <h5 class="card-title" style="font-weight: 600; font-size: 18px"></h5> -->
+        <p class="card-text" style="font-weight: 600; font-size: 20px; color: #666">Our relationship officer will contact you soon with more details</p>
+      </div>
+    </div>
+  </section>
+  
+
+  <!-- swiper js -->
+  <script src="assets/js/swiper-bundle.min.js"></script>
+  <script src="assets/js/custom-swiper.js"></script>
+
+  <!-- feather js -->
+  <script src="assets/js/feather.min.js"></script>
+  <script src="assets/js/custom-feather.js"></script>
+
+  <!-- iconsax js -->
+  <script src="assets/js/iconsax.js"></script>
+
+  <!-- bootstrap js -->
+  <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+  <!-- homescreen popup js -->
+  <script src="assets/js/homescreen-popup.js"></script>
+
+  <!-- PWA offcanvas popup js -->
+  <script src="assets/js/offcanvas-popup.js"></script>
+
+  <!-- script js -->
+  <script src="assets/js/script.js"></script>
+</body>
+
+
+<!-- Mirrored from themes.pixelstrap.net/pwa/mpay/landing.php by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 10 Sep 2024 05:07:49 GMT -->
+</html>
+</html>
